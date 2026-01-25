09b69b8f4a4c68be41c9d13929970d20bf9712c6
Atharv Shivale <161482155+String-Atharv@users.noreply.github.com>
2026-01-25 18:56:08 +0530

    Delete frontend directory

--- frontend/src/api/endpoints/roles.ts
+++
@@ -1,7 +0,0 @@
-import apiClient from '../client';
-
-export const rolesApi = {
-    promoteToOrganiser: async (): Promise<void> => {
-        await apiClient.post('/roles/promote-to-organiser');
-    },
-};
